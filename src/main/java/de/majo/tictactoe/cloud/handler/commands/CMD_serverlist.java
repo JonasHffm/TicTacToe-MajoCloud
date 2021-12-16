1428225ceed14b965c29eb4a173bc4dd7ddddbcf
JonasHffm <jonashoffmann1102@gmail.com>
2021-12-16 09:05:00 +0100

    CMD_serverlist.java: List of the logged in users in gameserver list

--- src/main/java/de/majo/tictactoe/cloud/handler/commands/CMD_serverlist.java
+++ src/main/java/de/majo/tictactoe/cloud/handler/commands/CMD_serverlist.java
@@ -9,6 +9,9 @@ public class CMD_serverlist extends CommandExecutor{
             if(!cloudServer.isProxyMode()) {
                 System.out.println(" > UID: " + cloudServer.getUID() + " - [" + cloudServer.getPort() + "] - User: " + cloudServer.getClient_list().size() + " / " + cloudServer.getMAX_CLIENT_ACCEPTION());
                 System.out.println("  >> " + cloudServer.getMotd());
+                if(cloudServer.getClient_list().values().size() > 0) {
+                    cloudServer.getClient_list().values().forEach(username -> System.out.println("  >> Logged in: " + username));
+                }
             }else {
                 System.out.println("Proxy-Server:");
                 System.out.println(" > UID: " + cloudServer.getUID() + " - [" + cloudServer.getPort() + "] - User: " + cloudServer.getClient_list().size() + " / " + 1000);
